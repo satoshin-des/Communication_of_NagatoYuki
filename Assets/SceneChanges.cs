82337f6bb43c46e1fbb1c94b67daa722f8d62320
Nagato <transcendentalliouville@gmail.com>
2024-07-06 21:49:24 +0900

    first commit

---
+++ Assets/SceneChanges.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanges : MonoBehaviour
+{
+    public void YuUtu()
+    {
+        SceneManager.LoadScene("YuUtuScene");
+    }
+
+    public void ShoShitu()
+    {
+        SceneManager.LoadScene("ShoShituScene");
+    }
+}
